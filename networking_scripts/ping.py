8d6beea5b8509c8b34285b5d264f58b3ed2348e6
rfdmeshkath <rfdmeshkath@gmail.com>
2018-11-07 01:47:33 +0000

    Basic working version of ping test

--- networking_scripts/ping.py
+++ networking_scripts/ping.py
@@ -1,24 +1,29 @@
-import os
+from multiping import MultiPing
+from time import sleep
 
 from alerts.message import send_message
 
 
-def check_ping(hostname):
-    response = os.system("ping " + hostname)
-    # checking for response
-    if response == 0:
-        pingstatus = 'Active'
-    else:
-        pingstatus = 'Down'
-
-    return pingstatus
-
-
-hostname = ''
+addresses = ["192.168.0.17"]
 while True:
-    status = check_ping(hostname)
-    if status == 'Down':
-        send_message(phone_no='+353874323320', message_body=hostname + ' is down.')
+    mp = MultiPing(addresses)
+
+    # Send the pings to those addresses
+    mp.send()
+
+    # With a 1 second timeout, wait for responses (may return sooner if all
+    # results are received.
+    responses, no_responses = mp.receive(1)
+    print(responses.keys(), no_responses)
+    sleep(5)
+    if no_responses:
+        print(no_responses)
+        # send_message(phone_no='+353874323320', message_body='\n {} device(s) is down'.format(' '.join(no_responses)))
         break
-    elif status == 'Active':
+    else:
         pass
+
+
+
+
+
