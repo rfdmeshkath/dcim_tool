b11700f881d4f2168d20f85971e67870798ae386
rfdmeshkath <rfdmeshkath@gmail.com>
2018-11-13 11:28:10 +0000

    first very basic working snmp version

---
+++ networking_scripts/snmp.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+from pysnmp.entity.rfc3413.oneliner import cmdgen
+
+COMMUNITY_STRING = 'fypreadsnmp'
+SNMP_PORT = 161
+SNMP_HOST = '192.168.11.2'
+sysUpTime_OID = '.1.3.6.1.2.1.1.3.0'
+sysDescr_OID = '.1.3.6.1.2.1.1.1.0'
+display_errors = True
+
+cmdGen = cmdgen.CommandGenerator()
+snmp_target = (SNMP_HOST, SNMP_PORT)
+cmd_gen = cmdgen.CommandGenerator()
+(error_detected, error_status, error_index, snmp_data) = \
+    cmd_gen.getCmd(cmdgen.CommunityData(COMMUNITY_STRING),
+                   cmdgen.UdpTransportTarget(snmp_target), sysUpTime_OID,
+                   lookupNames=True, lookupValues=True)
+if not error_detected:
+    print(snmp_data[0].prettyPrint())
+else:
+    if display_errors:
+        print('ERROR DETECTED: ')
+        print('    %-16s %-60s' % ('error_message', error_detected))
+        print('    %-16s %-60s' % ('error_status', error_status))
+        print('    %-16s %-60s' % ('error_index', error_index))
