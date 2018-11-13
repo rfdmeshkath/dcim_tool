4a94cbef994c969433f212c12ccb932de9be375e
rfdmeshkath <rfdmeshkath@gmail.com>
2018-11-13 11:28:32 +0000

    first very basic working ssh version

---
+++ networking_scripts/ssh.py
@@ -0,0 +1,12 @@
+from netmiko import ConnectHandler
+
+login_info = {
+  'device_type': 'cisco_ios',
+  'ip': '192.168.11.2',
+  'username': 'fyp',
+  'password': 'cisco',
+}
+device = ConnectHandler(**login_info)
+device.find_prompt()
+output = device.send_command('show ip int brief')
+print(output)
