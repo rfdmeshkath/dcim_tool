d866f265fa2f346cdb15a53967d522a3441e66a0
rfdmeshkath <rfdmeshkath@gmail.com>
2018-11-27 12:35:30 +0000

    initial table structure

--- database_helper/tables.sql
+++ database_helper/tables.sql
@@ -28,3 +28,63 @@ BEGIN
   FROM   dual;
 END;
 /
+
+------------------------------------------------------------------------------------------------
+CREATE TABLE device_status
+(
+    status_id           NUMBER(10)    NOT NULL,
+    device_name         VARCHAR2(50),
+    total_ram           NUMBER(5),
+    currently_used      NUMBER(5),
+    currently_free      NUMBER(5),
+    cpu_usage           NUMBER(3),
+    collection_date     VARCHAR2(50),
+    collection_time     VARCHAR2(50)
+);
+
+
+ALTER TABLE device_status ADD
+(
+    CONSTRAINT status_pk PRIMARY KEY (status_id)
+);
+
+DROP SEQUENCE status_seq;
+CREATE SEQUENCE status_seq START WITH 1;
+
+DROP TRIGGER status_trigger;
+CREATE OR REPLACE TRIGGER status_trigger
+BEFORE INSERT ON device_status
+FOR EACH ROW
+
+BEGIN
+  SELECT status_seq.NEXTVAL
+  INTO   :new.status_id
+  FROM   dual;
+END;
+/
+
+
+insert into device_status
+(
+    device_name,
+    total_ram,
+    currently_used,
+    currently_free,
+    cpu_usage,
+    collection_date,
+    collection_time
+)
+values
+(
+    'r1',
+     128,
+     50,
+     62,
+     15,
+    '12-23-1890',
+    '13:08'
+);
+
+
+
+select * from device_status;
