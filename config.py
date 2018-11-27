0f38f26a6bbd3383a5e2b0eed4f87d1042f1cd16
rfdmeshkath <rfdmeshkath@gmail.com>
2018-11-27 09:31:56 +0000

    changing app description in base_template

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 CISCO_LOGIN_INFO = {
-  'device_type': 'cisco_ios',
-  'ip': '',
-  'username': 'rafid',
-  'password': 'cisco',
+    'device_type': 'cisco_ios',
+    'ip': '',
+    'username': 'rafid',
+    'password': 'cisco',
 }
 
 DATABASE_CREDENTIAL = {
-    'user':'fyp_project',
-    'password':'fyp_project',
+    'user': 'fyp_project',
+    'password': 'fyp_project',
     'dsn': ''
 }
 DATABASE_DSN = {
@@ -15,3 +15,15 @@ DATABASE_DSN = {
     'port': '1521',
     'sid': 'xe'
 }
+
+
+
+TWILIO_DETAILS = {
+    'account_sid': 'AC7a988332a024a8ab8aeaa6526e2e4863',
+    'auth_token': '430b39388e991c940f3b5b2e1ffa4eb8',
+    'number': '+18507249675'
+
+}
+TWILIO_ACCOUNT_SID = 'AC7a988332a024a8ab8aeaa6526e2e4863'  # got this from Twilio Console Dashboard
+TWILIO_AUTH_TOKEN = '430b39388e991c940f3b5b2e1ffa4eb8'  # got this from Twilio Console Dashboard
+TWILIO_NUMBER = '+18507249675'
