6dbbbf8e4ca015fc85cc862096397799088de38d
rfdmeshkath <rfdmeshkath@gmail.com>
2018-11-27 09:50:45 +0000

    changing app description in base_template

--- alerts/send_mail.py
+++ alerts/send_mail.py
@@ -1,27 +1,27 @@
-def send_email(user, pwd, recipient, subject, body):
-    import smtplib
+import smtplib
 
-    FROM = user
+from config import MAIL_CREDENTIAL
+
+
+def send_email(recipient, subject, body):
     TO = recipient if isinstance(recipient, list) else [recipient]
     SUBJECT = subject
     TEXT = body
 
     # Prepare actual message
     message = """From: %s\nTo: %s\nSubject: %s\n\n%s
-    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    """ % (MAIL_CREDENTIAL['username'], ", ".join(TO), SUBJECT, TEXT)
     # try:
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.ehlo()
     server.starttls()
     server.ehlo()
-    server.login(user, pwd)
-    server.sendmail(FROM, TO, message)
+    server.login(MAIL_CREDENTIAL['username'], MAIL_CREDENTIAL['password'])
+    server.sendmail(MAIL_CREDENTIAL['username'], TO, message)
     server.close()
     print('successfully sent the alerts')
     # except:
     #     print ("failed to send alerts")
 
 
-for i in range(0, 1):
-    send_email('rafidmproject@gmail.com', 'FinalYearProject2019', 'rfdmeshkath@gmail.com',
-               'Automated Mail Header', 'Mail Body goes here.')
+send_email('rfdmeshkath@gmail.com', 'Automated Mail Header', 'Mail Body goes here.')
