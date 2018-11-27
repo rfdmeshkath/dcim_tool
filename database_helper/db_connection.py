11a09f7efe30ba0b3546e6f9a149111adc874171
rfdmeshkath <rfdmeshkath@gmail.com>
2018-11-27 12:37:54 +0000

    removing unnecessary codes

--- database_helper/db_connection.py
+++ database_helper/db_connection.py
@@ -3,11 +3,11 @@ import cx_Oracle
 from config import DATABASE_CREDENTIAL, DATABASE_DSN
 
 
-def db_connect(dsn, authentication):
+def db_connect():
     try:
-        dsn_string = cx_Oracle.makedsn(**dsn)
+        dsn_string = cx_Oracle.makedsn(**DATABASE_DSN)
         DATABASE_CREDENTIAL['dsn'] = dsn_string
-        connection = cx_Oracle.connect(**authentication)
+        connection = cx_Oracle.connect(**DATABASE_CREDENTIAL)
         cursor = connection.cursor()
     except Exception as e:
         connection = 'Error-501: Database connection error'
@@ -16,11 +16,11 @@ def db_connect(dsn, authentication):
     return connection, cursor
 
 
-connection,cursor = db_connect(DATABASE_DSN, DATABASE_CREDENTIAL)
-temp_var = cursor.execute('select * from connections')
-
-result = temp_var.fetchall()
-
-print(result, type(result))
-cursor.close()
-connection.close()
+# connection,cursor = db_connect()
+# temp_var = cursor.execute('select * from connections')
+#
+# result = temp_var.fetchall()
+#
+# print(result, type(result))
+# cursor.close()
+# connection.close()
