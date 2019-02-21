0ae3b7072cd845b2b3960207521f231dfe19ae96
rfdmeshkath <rfdmeshkath@gmail.com>
2019-02-21 02:46:54 +0000

    added comments

--- networking_scripts/details_info_collector.py
+++ networking_scripts/details_info_collector.py
@@ -4,35 +4,56 @@ from networking_scripts.device_scripts import Cisco_3700
 from networking_scripts.snmp import collect_snmp_data
 
 for device in all_devices:
-    os_description = collect_snmp_data(device, SNMP_OID['sysDescr']).split('\n')[0].split('=')[1].strip()
+    os_description = collect_snmp_data(device, SNMP_OID['sysDescr'])
 
     if 'Cisco' in os_description:
-        connection_df, ram_df, cpu_df = Cisco_3700(device).formatted_output()
+        connection_df, ram_df, cpu_df, unused_port_df, error_df = Cisco_3700(device).formatted_output()
 
-        # print('\n', connection_df, '\n', ram_df, '\n', cpu_df)
         # update or insert these data to database
         con_status = execute_procedure('prc_insert_lldp_connection',
                                        [
-                                        connection_df['local_device'][0],
-                                        connection_df['local_port'][0],
-                                        connection_df['remote_device'][0],
-                                        connection_df['remote_port'][0],
-                                        connection_df['date_time'][0]
+                                           connection_df['local_device'][0],
+                                           connection_df['local_port'][0],
+                                           connection_df['remote_device'][0],
+                                           connection_df['remote_port'][0],
+                                           connection_df['date_time'][0]
                                        ])
         if con_status != 'success':
             print('failed ' + con_status)
-        else:
-            print(con_status)
 
         mem_status = execute_procedure('prc_insert_memory_usage',
                                        [
-                                        ram_df['device_name'][0],
-                                        int(ram_df['currently_used'][0]),
-                                        int(cpu_df['cpu_used'][0]),
-                                        cpu_df['date_time'][0]
+                                           ram_df['device_name'][0],
+                                           int(ram_df['currently_used'][0]),
+                                           int(cpu_df['cpu_used'][0]),
+                                           cpu_df['date_time'][0]
                                        ])
 
         if mem_status != 'success':
             print('failed ' + mem_status)
-        else:
-            print(mem_status)
+
+        unused_port_status = execute_procedure('prc_insert_unused_port',
+                                               [
+                                                   unused_port_df['device_name'][0],
+                                                   unused_port_df['port_name'][0],
+                                                   unused_port_df['date_time'][0]
+                                               ])
+
+        if unused_port_status != 'success':
+            print('failed ' + unused_port_status)
+
+        for index, row in error_df.iterrows():
+            port_error_status = execute_procedure('prc_insert_port_error',
+                                                  [
+                                                      row['device_name'],
+                                                      row['port'],
+                                                      int(row['input']),
+                                                      int(row['crc']),
+                                                      int(row['frame']),
+                                                      int(row['overrun']),
+                                                      int(row['ignored']),
+                                                      row['date_time'],
+                                                  ])
+
+            if port_error_status != 'success':
+                print('failed ' + port_error_status)
--- networking_scripts/device_scripts.py
+++ networking_scripts/device_scripts.py
@@ -7,6 +7,10 @@ from config import CISCO_LOGIN_INFO
 
 
 def get_date_time():
+    """
+    This function gets current date and time and formats the information as per database requirement
+    :return: string dd-mm-yyyy hh:mm
+    """
     d = datetime.datetime.now().date()
     t = datetime.datetime.now().time()
     date = str(d.day) + '-' + str(d.month) + '-' + str(d.year)
@@ -20,6 +24,10 @@ class Cisco_3700:
         self.device = device
 
     def get_total_ram(self):
+        """
+        This function connects to a device and retrieves ram usage
+        :return: int (RAM usage in megabyte)
+        """
         CISCO_LOGIN_INFO['device_type'] = 'cisco_ios'
         CISCO_LOGIN_INFO['ip'] = self.device
         # add try catch
@@ -30,6 +38,11 @@ class Cisco_3700:
         return round(ram_df['total_ram'].to_list()[0])
 
     def retrieve_data(self, device):
+        """
+        This function connects to a device and retrieves current "LLDP neighbor", CPU usage and RAM usage information
+        :param device: device name of a device
+        :return: pandas DataFrame
+        """
         CISCO_LOGIN_INFO['device_type'] = 'cisco_ios'
         CISCO_LOGIN_INFO['ip'] = device
         # add try catch
@@ -44,10 +57,9 @@ class Cisco_3700:
 
     def lldp_neighbour(self, output_str):
         """
-        This function takes the raw output of 'show cdp neighbour' in cisco routers
-        and returns a pandas DataFrame of column 'local_device', 'local_port', 'remote_device', 'remote_port',
-        'date', 'time'
-        :param output_str: (type: str) raw output generated from the command 'show cdp neighbour'
+        This function takes the raw output of a device which is a string and extract information from that
+        string and makes a DataFrame
+        :param output_str: string - LLDP neighbor output generated from a CISCO 3700 series device
         :return: pandas DataFrame
         """
 
@@ -71,10 +83,9 @@ class Cisco_3700:
 
     def ram_usage(self, output_str):
         """
-        This function takes the raw output of 'show processes memory | i Processor' in cisco routers
-        and returns a pandas DataFrame of column 'device_name', 'total_ram', 'currently_used', 'currently_free',
-        'date', 'time'
-        :param output_str: (type: str) raw output generated from the command 'show processes memory | i Processor'
+        This function takes the raw RAM usage output from a networking device, then it extracts necessary data
+         and then converts it to a DataFrame
+        :param output_str: RAM usage output generated from a CISCO 3700 series device
         :return: pandas DataFrame
         """
 
@@ -90,9 +101,9 @@ class Cisco_3700:
 
     def cpu_usage(self, output_str):
         """
-        This function takes the raw output of 'show processes cpu sorted | i CPU' in cisco routers
-        and returns a pandas DataFrame of column 'device_name', 'cpu_used', 'date', 'time'
-        :param output_str: (type: str) raw output generated from the command 'show processes cpu sorted | i CPU'
+        This function takes the raw CPU usage output from a networking device, then it extracts necessary data
+         and then converts it to a DataFrame
+        :param output_str: CPU usage output generated from a cisco 3700 series device
         :return: pandas DataFrame
         """
 
@@ -103,11 +114,18 @@ class Cisco_3700:
         return cpu_df
 
     def get_errors(self, output_str):
+        """
+        This function extracts five types of errors (input,crc, overrun, frame, ignored) that might generate from a
+        networking device.
+        :param:port error output generated from a cisco 3700 series device
+        :return: pandas DataFrame
+        """
         date_time = get_date_time()
-        error_df = pd.DataFrame(columns=['device_name', 'input', 'crc', 'frame', 'overrun', 'ignored', 'date_time'])
         trimmed_output = []
+        all_errors = []
         line_counter = 0
         temp_lines = ''
+        # trimming unnecessary lines from raw input
         for line in output_str.split('\n'):
             if 'protocol' in line or 'input errors' in line:
                 line_counter = line_counter + 1
@@ -117,28 +135,39 @@ class Cisco_3700:
                     line_counter = 0
                     temp_lines = ''
 
+        # extracting necessary information from each trimmed line
         for line in trimmed_output:
+            # extracting port name
             port = re.search('(FastEthernet|Serial)\d*\W\d*', line).group(0)
             error_str = re.search('\d* input errors', line).group(0)
             input_error = re.search('\d*', error_str).group(0)
-
+            # extracting crc error
             error_str = re.search('\d* CRC', line).group(0)
             crc_error = re.search('\d*', error_str).group(0)
-
+            # extracting frame error
             error_str = re.search('\d* frame', line).group(0)
             frame_error = re.search('\d*', error_str).group(0)
-
+            # extracting overrun error
             error_str = re.search('\d* overrun', line).group(0)
             overrun_error = re.search('\d*', error_str).group(0)
-
+            # extracting ignored error
             error_str = re.search('\d* ignored', line).group(0)
             ignored_error = re.search('\d*', error_str).group(0)
+            # appending to a temporary list which will be later converted to a DataFrame
+            all_errors.append([self.device, port, input_error, crc_error, frame_error, overrun_error,
+                               ignored_error, date_time])
 
-            error_df = error_df.append(pd.DataFrame([[port, input_error, crc_error, frame_error, overrun_error,
-                                                      ignored_error, date_time]], columns=error_df.columns))
+        error_df = pd.DataFrame(all_errors, columns=['device_name', 'port', 'input', 'crc', 'frame', 'overrun',
+                                                     'ignored', 'date_time'])
         return error_df
 
     def get_unused_ports(self, unused_port_str):
+        """
+        This function extracts all the unused ports of a device
+        :param unused_port_str: raw output of unused ports from a cisco 3700 series device
+        :return: pandas DataFrame
+        """
+        # if empty sting is passed then return empty DataFrame
         if unused_port_str == '':
             return pd.DataFrame()
 
@@ -155,8 +184,7 @@ class Cisco_3700:
         connection_df = self.lldp_neighbour(connections_str)
         ram_df = self.ram_usage(ram_str)
         cpu_df = self.cpu_usage(cpu_str)
-        error_df = self.get_errors(error_str)
         unused_port_df = self.get_unused_ports(unused_port_str)
-        print(error_df, '\n', unused_port_df)
-        return connection_df, ram_df, cpu_df
+        error_df = self.get_errors(error_str)
 
+        return connection_df, ram_df, cpu_df, unused_port_df, error_df
