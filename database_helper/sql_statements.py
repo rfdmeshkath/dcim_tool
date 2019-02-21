1658190074ffb87fd1588261fd2c8da999635914
rfdmeshkath <rfdmeshkath@gmail.com>
2019-02-21 02:24:55 +0000

    few sql statements are modified

--- database_helper/sql_statements.py
+++ database_helper/sql_statements.py
@@ -65,6 +65,164 @@ def select_device_info(device_name):
     return select_statement
 
 
+def select_unused_ports(device_name):
+    select_statement = '''
+    SELECT 
+        x.device_name        device_name, 
+        a.port_name          port_name,
+        a.updated_datetime   date_time
+    FROM 
+        device x, 
+        unused_port a
+    
+    WHERE 
+        a.p_device_id = x.device_id
+        AND 
+        x.device_name = '{}'
+        AND
+        a.updated_datetime = (SELECT MAX(updated_datetime) FROM unused_port)
+        '''.format(device_name)
+    return select_statement
+
+
+def select_port_error(device_name):
+    select_statement = '''
+    SELECT 
+        a.port_name          port_name,
+        a.input_error        input_error,
+        a.crc_error          crc_error,
+        a.frame_error        frame_error,
+        a.overrun_error      overrun_error,
+        a.ignored_error      ignored_error,
+        a.updated_datetime   date_time
+    FROM 
+        device x, 
+        port_error a
+    WHERE 
+        a.e_device_id = x.device_id
+        AND 
+        x.device_name = '{}'
+        AND
+        a.updated_datetime = (SELECT MAX(updated_datetime) FROM port_error)
+    '''.format(device_name)
+    return select_statement
+
+
+def select_input_error(device_name):
+    select_statement = '''
+    SELECT 
+        x.device_name        device_name,
+        a.port_name          port_name,
+        a.input_error        input_error,
+        a.updated_datetime   date_time
+
+    FROM 
+        device x, 
+        port_error a
+    WHERE 
+        a.e_device_id = x.device_id
+        AND 
+        x.device_name like '{}'
+        AND
+        a.updated_datetime = (SELECT MAX(updated_datetime) FROM port_error)
+        AND
+        input_error > 0 
+        '''.format(device_name)
+    return select_statement
+
+
+def select_crc_error(device_name):
+    select_statement = '''
+    SELECT 
+        x.device_name        device_name,
+        a.port_name          port_name,
+        a.crc_error          crc_error,
+        a.updated_datetime   date_time
+
+    FROM 
+        device x, 
+        port_error a
+    WHERE 
+        a.e_device_id = x.device_id
+        AND 
+        x.device_name like '{}'
+        AND
+        a.updated_datetime = (SELECT MAX(updated_datetime) FROM port_error)
+        AND
+        crc_error > 0
+        '''.format(device_name)
+    return select_statement
+
+
+def select_frame_error(device_name):
+    select_statement = '''
+    SELECT 
+        x.device_name        device_name,
+        a.port_name          port_name,
+        a.frame_error        frame_error,
+        a.updated_datetime   date_time
+
+    FROM 
+        device x, 
+        port_error a
+    WHERE 
+        a.e_device_id = x.device_id
+        AND 
+        x.device_name like '{}'
+        AND
+        a.updated_datetime = (SELECT MAX(updated_datetime) FROM port_error)
+        AND
+        frame_error > 0
+        '''.format(device_name)
+    return select_statement
+
+
+def select_overrun_error(device_name):
+    select_statement = '''
+    SELECT 
+        x.device_name        device_name,
+        a.port_name          port_name,
+        a.overrun_error      overrun_error,
+        a.updated_datetime   date_time
+
+    FROM 
+        device x, 
+        port_error a
+    WHERE 
+        a.e_device_id = x.device_id
+        AND 
+        x.device_name like '{}'
+        AND
+        a.updated_datetime = (SELECT MAX(updated_datetime) FROM port_error)
+        AND
+        overrun_error > 0
+        '''.format(device_name)
+    return select_statement
+
+
+def select_ignored_error(device_name):
+    select_statement = '''
+    SELECT 
+        x.device_name        device_name,
+        a.port_name          port_name,
+        a.ignored_error      ignored_error,
+        a.updated_datetime   date_time
+
+    FROM 
+        device x, 
+        port_error a
+    WHERE 
+        a.e_device_id = x.device_id
+        AND 
+        x.device_name like 'R1'
+        AND
+        a.updated_datetime = (SELECT MAX(updated_datetime) FROM port_error)
+        AND
+        ignored_error > 0
+        '''.format(device_name)
+    return select_statement
+
+
 a = '''SELECT 
     x.device_name, 
     a.local_port
@@ -76,5 +234,5 @@ WHERE
     AND
     device_name='local-device-1'
     AND
-    local_port='eth1';
+    local_port='eth1'
 '''
