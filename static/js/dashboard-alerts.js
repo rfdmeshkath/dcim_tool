ce1dd19c1b2cbaf97ebc922d41c6aed6e38383a5
rfdmeshkath <rfdmeshkath@gmail.com>
2019-02-23 00:32:05 +0000

    added new javascript file for dashboard

---
+++ static/js/dashboard-alerts.js
@@ -0,0 +1,46 @@
+getAlertsInfo();
+setInterval(getAlertsInfo, 1000); //3000 MS == 3 seconds
+
+var alertBody = document.getElementById('alert-body');
+
+function getAlertsInfo() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://rafid/api-alerts-info',
+        success: function(alerts) {
+            alertBody.innerHTML = '';
+            $.each(alerts, function (i, alert) {
+                var link = '/resolve-alert/' + alert.id;
+                //console.log(link);
+                if (alert.severity === 'alert'){
+                    alertBody.innerHTML += '<div class="alert alert-info" role="alert"><strong>Device: ' +
+                                            '</strong>'+ alert.device_name +
+                                            ' || <strong>Message: </strong>' + alert.error_details +
+                                            ' || <strong>Occured: </strong>' + alert.date_time + '    ' +
+                                            '<a href=" '+ link +'"class="btn btn-outline-secondary btn-sm" ' +
+                                            'style="float: right;" role="button">Resolve</a>' +
+                                            '</div>';
+                } else if (alert.severity === 'warning'){
+                    alertBody.innerHTML += '<div class="alert alert-warning" role="alert"><strong>Device: ' +
+                                            '</strong>'+ alert.device_name +
+                                            ' || <strong>Message: </strong>' + alert.error_details +
+                                            ' || <strong>Occured: </strong>' + alert.date_time + '    ' +
+                                            '<a href=" '+ link +'"class="btn btn-outline-secondary btn-sm" ' +
+                                            'style="float: right;" role="button">Resolve</a>' +
+                                            '</div>';
+
+                } else if(alert.severity === 'danger'){
+                    alertBody.innerHTML += '<div class="alert alert-danger" role="alert"><strong>Device: ' +
+                                            '</strong>'+ alert.device_name +
+                                            ' || <strong>Message: </strong>' + alert.error_details +
+                                            ' || <strong>Occured: </strong>' + alert.date_time + '    ' +
+                                            '<a href=" '+ link +'"class="btn btn-outline-secondary btn-sm" ' +
+                                            'style="float: right;" role="button">Resolve</a>' +
+                                            '</div>';
+                }else {
+                    console.log('severity type error');
+                }
+            });
+        }
+    });
+}
--- static/js/notification-updater.js
+++ static/js/notification-updater.js
@@ -4,7 +4,7 @@ setInterval(getTotalAlerts, 30000); //3000 MS == 3 seconds
     function getTotalAlerts() {
         $.ajax({
             type: 'GET',
-            url: 'http://rafid/total-dashboard-alerts',
+            url: 'http://rafid/api-total-alerts',
             success: function(data) {
                 console.log('success', data);
                 document.getElementById("alert_notification").innerHTML=data;
