1b77128809bae6fbc34a9c8abca14f2ee3463bbc
rfdmeshkath <rfdmeshkath@gmail.com>
2019-03-07 00:47:42 +0000

    config added

---
+++ config.py
@@ -0,0 +1,60 @@
+CISCO_LOGIN_INFO = {
+    'device_type': '',
+    'ip': '',
+    'username': 'rafid',
+    'password': 'cisco',
+}
+
+DATABASE_CREDENTIAL = {
+    'user': 'fyp_project',
+    'password': 'fyp_project',
+    'dsn': ''
+}
+
+DATABASE_DSN = {
+    'host': 'localhost',
+    'port': '1521',
+    'sid': 'xe'
+}
+
+TWILIO_DETAILS = {
+    'account_sid': 'AC7a988332a024a8ab8aeaa6526e2e4863',
+    'auth_token': '430b39388e991c940f3b5b2e1ffa4eb8',
+    'number': '+18507249675'
+
+}
+
+MAIL_CREDENTIAL = {
+    'username': 'rafidmproject@gmail.com',
+    'password': 'FinalYearProject2019'
+}
+
+SNMP_DETAILS = {
+    'community_string': 'fypreadsnmp',
+    'port': 161
+}
+
+SNMP_OID = {
+    'sysDescr': '.1.3.6.1.2.1.1.1.0',
+    'sysUpTime': '.1.3.6.1.2.1.1.3.0',
+    'sysName': '.1.3.6.1.2.1.1.5.0'
+}
+
+ERROR_MESSAGES = {
+    'database_connection_error': 'Error-501: Database connection error'
+}
+
+# SEVERITY type: alert, warning, danger
+SEVERITY = {
+    'port_error': 'warning',
+    'host_unreachable': 'danger'
+}
+
+MONITOR_PHONE = ['+353874323320']
+
+MONITOR_EMAIL = ['rfdmeshkath@gmail.com']
+
+all_devices = ['R1']
+
+
+
