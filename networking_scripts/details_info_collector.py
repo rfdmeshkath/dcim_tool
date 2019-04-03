f8f104face8172b6a8bbf6b844981e306948292b
rfdmeshkath <rfdmeshkath@gmail.com>
2019-04-03 18:06:30 +0100

    new device cisco 2900 series added

--- networking_scripts/details_info_collector.py
+++ networking_scripts/details_info_collector.py
@@ -1,27 +1,42 @@
+import pandas as pd
+
 from config import all_devices, SNMP_OID
 from database_helper.procedures import execute_procedure
-from networking_scripts.device_scripts import Cisco_3700
+from networking_scripts.device_scripts import Cisco_3700, Cisco_2900
 from networking_scripts.snmp import collect_snmp_data
 
 for device in all_devices:
     os_description = collect_snmp_data(device, SNMP_OID['sysDescr'])
+    if os_description == '':
+        print('could not establish connection')
+        break
 
-    if 'Cisco' in os_description:
+    connection_df = pd.DataFrame()
+    ram_df = pd.DataFrame()
+    cpu_df = pd.DataFrame()
+    unused_port_df = pd.DataFrame()
+    error_df = pd.DataFrame()
+
+    if 'Cisco' in os_description and 'C2900' in os_description:
+        connection_df, ram_df, cpu_df, unused_port_df, error_df = Cisco_2900(device).formatted_output()
+    elif 'Cisco' in os_description and '3700' in os_description:
         connection_df, ram_df, cpu_df, unused_port_df, error_df = Cisco_3700(device).formatted_output()
 
-        # update or insert these data to database
+    # update or insert these data to database
+    if not connection_df.empty:
         for index, row in connection_df.iterrows():
             insert_status = execute_procedure('prc_insert_lldp_connection',
-                                           [
-                                               row['local_device'],
-                                               row['local_port'],
-                                               row['remote_device'],
-                                               row['remote_port'],
-                                               row['date_time']
-                                           ])
+                                              [
+                                                  row['local_device'],
+                                                  row['local_port'],
+                                                  row['remote_device'],
+                                                  row['remote_port'],
+                                                  row['date_time']
+                                              ])
             if insert_status != 'success':
                 print('failed ' + insert_status)
 
+    if not ram_df.empty:
         mem_status = execute_procedure('prc_insert_memory_usage',
                                        [
                                            ram_df['device_name'][0],
@@ -33,16 +48,19 @@ for device in all_devices:
         if mem_status != 'success':
             print('failed ' + mem_status)
 
-        unused_port_status = execute_procedure('prc_insert_unused_port',
-                                               [
-                                                   unused_port_df['device_name'][0],
-                                                   unused_port_df['port_name'][0],
-                                                   unused_port_df['date_time'][0]
-                                               ])
+    if not unused_port_df.empty:
+        for index, row in unused_port_df.iterrows():
+            unused_port_status = execute_procedure('prc_insert_unused_port',
+                                                   [
+                                                       row['device_name'],
+                                                       row['port_name'],
+                                                       row['date_time']
+                                                   ])
 
-        if unused_port_status != 'success':
-            print('failed ' + unused_port_status)
+            if unused_port_status != 'success':
+                print('failed ' + unused_port_status)
 
+    if not error_df.empty:
         for index, row in error_df.iterrows():
             port_error_status = execute_procedure('prc_insert_port_error',
                                                   [
@@ -60,4 +78,4 @@ for device in all_devices:
                 print('failed ' + port_error_status)
 
     else:
-        print('Failed to connect to device {}'.format(device))
+        print('data extractor class is not present for {}'.format(device))
